573799607e198571d4ccadbd539ba839713b6458
Henrique Carlos <henriquecarlos361893@gmail.com>
2024-04-09 13:52:54 -0300

    Criação do formulário de agendamento

--- scripts/dist/script.js
+++ scripts/dist/script.js
@@ -26,3 +26,42 @@ cardButtonEmail.addEventListener("mouseover", () => {
 cardButtonEmail.addEventListener("mouseout", () => {
     cardButtonEmail.innerHTML = "NOSSO EMAIL";
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const timeSelect = document.querySelector("#time");
+    const dateInput = document.querySelector("#datetime");
+    function updateTimes() {
+        timeSelect.innerHTML = '';
+        const dateSelected = new Date(dateInput.value + 'T00:00:00-03:00');
+        const atualTime = new Date();
+        let month = atualTime.getMonth() + 1;
+        let day = atualTime.getDate(); // Correção aqui
+        const year = atualTime.getFullYear();
+        if (month < 10) {
+            month = '0' + month.toString();
+        }
+        if (day < 10) {
+            day = '0' + day.toString();
+        }
+        const maxDate = year + '-' + month + '-' + day;
+        dateInput.setAttribute('min', maxDate);
+        let initialTime = new Date(dateSelected);
+        initialTime.setHours(8);
+        initialTime.setMinutes(0);
+        const finalTime = new Date(dateSelected);
+        finalTime.setHours(18);
+        finalTime.setMinutes(0);
+        const interval = 1;
+        let time = new Date(initialTime);
+        while (time <= finalTime) {
+            if (!(dateSelected.toDateString() === atualTime.toDateString() && time <= atualTime)) {
+                const option = document.createElement('option');
+                option.value = time.getHours() + ':' + (time.getMinutes() < 10 ? '0' : '') + time.getMinutes();
+                option.textContent = option.value;
+                timeSelect.appendChild(option);
+            }
+            time.setHours(time.getHours() + interval);
+        }
+    }
+    dateInput.addEventListener('change', updateTimes);
+    updateTimes();
+});
